3a204a9cef8d20a5ea2e200d677f685da31246f0
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2022-06-03 12:21:42 -0500

    Se termina diseño de la prueba y se sincroniza con la API

--- src/Components/TodayPlans_BodyRight/TodayPlans_BodyRight.jsx
+++ src/Components/TodayPlans_BodyRight/TodayPlans_BodyRight.jsx
@@ -5,6 +5,8 @@ import TodayPlans_BodyRightCard from '../TodayPlans_BodyRightCard/TodayPlans_Bod
 const TodayPlans_BodyRight = () => {
 	return (
 		<div className='TodayPlans_BodyRight'>
+			<TodayPlans_BodyRightCard number={10} descriptionTitle='Leads de Primer Contacto' />
+			<TodayPlans_BodyRightCard number={20} descriptionTitle='Leads de Primer Contacto' />
 			<TodayPlans_BodyRightCard number={30} descriptionTitle='Leads de Primer Contacto' />
 		</div>
 	)
